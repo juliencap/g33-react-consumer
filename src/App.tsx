de1d7ea656bac5aa2e00168881582e421680fb6b
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-06-03 22:35:12 +0200

    hero-banner from design-system

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ function App() {
   return (
     <>
       <h1>React Consumer</h1>
-
-      <g33-button variant="primary">Test depuis React</g33-button>
+      <g33-hero-banner
+        title-text="Girondins33 Design System"
+        subtitle="Consommé depuis React"
+        image-url="https://www.girondins33.com/storage/2024/02/matmut-atlantique-girondins-sc-1-800x445.webp"
+        image-alt="Logo Girondins33"
+        cta-label="Découvrir"
+        cta-url="https://www.girondins33.com"
+      />
     </>
   );
 }
