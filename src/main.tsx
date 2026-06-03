6061ed118240e7d32ec2c0a4c9a9f2003880331f
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-06-03 21:57:04 +0200

    feat: integrate Stencil design system in React

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { defineCustomElements } from 'girondins33-design-system/loader';
+
+defineCustomElements();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["girondins33-design-system"],
+  },
+});
